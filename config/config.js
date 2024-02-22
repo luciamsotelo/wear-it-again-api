b6c3de48c9412eb5306617578f6b7b8149ede684
luciamsotelo <142464028+luciamsotelo@users.noreply.github.com>
2024-02-21 18:51:48 -0800

    creating railway data

--- config/config.js
+++ config/config.js
@@ -1,23 +1,23 @@
 module.exports = {
         development: {
                 username: "postgres",
-                password: "new_password",
-                database: "WearItAgain",
-                host: "127.0.0.1",
+                password: "Bef4E6Bb5eG6D1cG2B2E*gaCd6bB62eE",
+                database: "railway",
+                host: "monorail.proxy.rlwy.net",
                 dialect: "postgres",
         },
         test: {
                 username: "postgres",
-                password: "new_password",
-                database: "WearItAgain",
-                host: "127.0.0.1",
+                password: "Bef4E6Bb5eG6D1cG2B2E*gaCd6bB62eE",
+                database: "railway",
+                host: "monorail.proxy.rlwy.net",
                 dialect: "postgres",
         },
         production: {
                 username: "postgres",
-                password: "new_password",
-                database: "WearItAgain",
-                host: "127.0.0.1",
+                password: "Bef4E6Bb5eG6D1cG2B2E*gaCd6bB62eE",
+                database: "railway",
+                host: "monorail.proxy.rlwy.net",
                 dialect: "postgres",
         },
 };
