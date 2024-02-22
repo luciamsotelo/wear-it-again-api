162d2212f25aff65a1c23a4a2ef7fe928d3abbfb
luciamsotelo <142464028+luciamsotelo@users.noreply.github.com>
2024-02-21 18:25:37 -0800

    worked on render

--- config/config.js
+++ config/config.js
@@ -1,25 +1,26 @@
 module.exports = {
         development: {
-                // username: "postgres",
-                // password: "new_password",
-                // database: "WearItAgain",
-                // host: "127.0.0.1",
-                database: "wear_it_again",
-                connectionString: "DATABASE_URL",
+                username: "postgres",
+                password: "new_password",
+                database: "WearItAgain",
+                host: "127.0.0.1",
                 dialect: "postgres",
         },
         test: {
-                database: "wear_it_again",
-                connectionString: "DATABASE_URL",
+                username: "postgres",
+                password: "new_password",
+                database: "WearItAgain",
+                host: "127.0.0.1",
                 dialect: "postgres",
         },
         production: {
-                database: "wear_it_again",
-                connectionString: "DATABASE_URL",
+                username: "postgres",
+                password: "new_password",
+                database: "WearItAgain",
+                host: "127.0.0.1",
                 dialect: "postgres",
-        }
+        },
 };
 
 
 
-// postgres://wear_it_again_user:erVXXZmhFPeZTRAEKplLWGXolvetOvIf@dpg-cnakj60l6cac739ov3pg-a/wear_it_again
--- models/index.js
+++ models/index.js
@@ -10,9 +10,8 @@ const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
-if (config.connectionString) {
-  // console.log(process.env[config.connectionString])
-  sequelize = new Sequelize(process.env[config.connectionString], config);
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
